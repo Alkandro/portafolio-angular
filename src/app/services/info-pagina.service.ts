a6ff4658cfb5f7f1b056c33ad232abf0020402c1
Alkandro <alkandro@hotmail.com>
2019-11-19 22:09:29 +0900

    clase 40

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,22 +9,31 @@ export class InfoPaginaService {
 
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
-  constructor( private http: HttpClient) { 
-    console.log('servicio de infoPaginas cargadas');
 
+  constructor( private http: HttpClient) {
+
+  this.cargarInfo();
+  this.cargarEquipo();
+
+  
+  }
+  private cargarInfo() {
 //leer el archivo JSON
 this.http.get('assets/data/data-pagina-json')
         .subscribe( (resp: InfoPagina ) => {
-
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
+          
           });
-
-
-
-
-    
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-1a4cc.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[]) => {
+          
+          this.equipo = resp;
+  });
+}
 }
